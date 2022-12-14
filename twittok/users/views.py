ac7c9f67ec0ffaff20a8dd57cafeeba65a8cb11a
boris1592 <buzin.boria@gmail.com>
2022-12-14 11:04:19 +0300

    fixed redirect after signup

--- twittok/users/views.py
+++ twittok/users/views.py
@@ -1,18 +1,21 @@
 from django.views.generic import FormView
-from .forms import SignupForm
+from django.contrib.auth import login
 from django.contrib.auth.models import User
+
+from .forms import SignupForm
 
 
 class SignupView(FormView):
     form_class = SignupForm
     template_name = 'form_page.html'
-    success_url = '/users/login'
+    success_url = '/users/profile'
 
     def form_valid(self, form):
-        User.objects.create_user(
+        user = User.objects.create_user(
             username=form.cleaned_data['username'],
             email=form.cleaned_data['username'],
             password=form.cleaned_data['password'],
             first_name=form.cleaned_data['first_name'],
         )
+        login(self.request, user)
         return super().form_valid(form)
