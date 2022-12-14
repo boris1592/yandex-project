71eb868da025f0a20e92fc2731053b7002420517
boris1592 <buzin.boria@gmail.com>
2022-12-14 11:34:24 +0300

    created models

--- twittok/posts/models.py
+++ twittok/posts/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+
+class TagStats(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
